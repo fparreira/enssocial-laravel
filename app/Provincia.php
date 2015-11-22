ff287c909f9da1857af637fa39962bd86a766ef2
fparreira <fparreira@gmail.com>
2015-11-22 13:39:40 -0200

    Inicio repositorio

---
+++ app/Provincia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Provincia extends Model
+{
+
+  protected $table = 'provincia';
+  protected $primaryKey = 'id_pro';
+
+}
---
+++ app/Regiao.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Regiao extends Model
+{
+    protected $table = 'regiao';
+    protected $primaryKey = 'id_reg';
+
+    
+}
